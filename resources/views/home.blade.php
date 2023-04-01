0f2b8f42f261d9f341a00567c93dc9cd6ecd1e4a
khadija-bibi <sub99way1122@gmail.com>
2023-04-01 16:53:06 +0500

    additional formatting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
     <script src="{{ asset('js/all.min.js') }}"></script>
   <meta charset="utf-8">
@@ -8,14 +9,14 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-  <nav class="bg-gray-900">
+  <nav class="bg-cyan-900 ">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+              <a href="/" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
                </div>
           </div>
         </div>
@@ -23,7 +24,7 @@
           <div class="flex space-x-4">
             <form method="POST" action="{{ route('logout') }}" role="button">
               @csrf
-              <a :href="route('logout')" class="nav-link text-gray-300 hover:bg-cyan-700 hover:text-white" text-gray-300 onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+              <a :href="route('logout')" class="nav-link text-white hover:bg-transparent hover:bg-cyan-700 hover:text-white" text-gray-300 onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
           </form>
           </div>
         </div>
@@ -33,8 +34,8 @@
   
     <div class="flex justify-center items-center h-screen  bg-cyan-600">
         <div class="w-400 p-6 shadow-lg bg-white rounded-md">
-            <h1 class="text-3xl block text-center front-semibold  text-cyan-700 ">
-                TODO  LIST  APP
+            <h1 class="text-3xl block text-center front-semibold  text-cyan-900 ">
+                __TODO  LIST  APP__
             </h1>
             <hr class="mt-3">
             <form action="{{ route('store')}}" method="POST" autocomplete="off">
@@ -60,7 +61,7 @@
                     <div>
                       @csrf
                       @method('delete') 
-                      <button type="submit" class="bg-gray-800 text-white rounded-lg px-2 py-1">Delete</button>
+                      <button type="submit px-5" ><i class="fas fa-trash color  text-red-500"></i></button>
                     </div>
                   </div>
                  
